ce10de79e52484d917cad4850d567d8b7a961ce2
mathiuSpv <mathius998@gmail.com>
2024-10-07 12:18:37 -0300

    feat: nuevo feature agregado temporizador de experiencia

--- cogs/time_tracker.py
+++ cogs/time_tracker.py
@@ -1,6 +1,10 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from discord import Member, VoiceState
 from discord.ext.commands import Cog, Bot
 from datetime import datetime, timedelta
+import data.sql_function as sql
 
 class TimeTrackerCog(Cog):
     def __init__(self, bot: Bot):
@@ -17,17 +21,19 @@ class TimeTrackerCog(Cog):
                 entry_time = self.user_entry_time.pop(member.id)
                 exit_time = datetime.now()
                 time_diff : timedelta = (exit_time  - entry_time)
-                print(f"{member.name} : {time_diff.total_seconds()}")
-                print(self.user_entry_time)
+                second_experience : int = int(time_diff.total_seconds())
+                exist, _ = sql.getUser(member.id)
+                if (exist):
+                    sql.updateUserExp(member.id, second_experience)
+                else:
+                    sql.insertUser(member.id, second_experience)
+                
     
 async def setup(bot: Bot):
     await bot.add_cog(TimeTrackerCog(bot))
 
 if __name__ == "__main__":
     import asyncio, os, sys
-
-    # Añadir la ruta principal al sys.path
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  
 
     # Importar la función cog_start desde el archivo principal
     from bot import cog_start
